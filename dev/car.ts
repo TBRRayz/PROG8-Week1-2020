e2bf564bb33746a379f471b12b1ea5a17250f8c0
Bryan <bryanderonde@hotmail.com>
2020-05-09 11:49:05 +0200

    make the car clickable

--- dev/car.ts
+++ dev/car.ts
@@ -13,17 +13,28 @@ class Car extends HTMLElement{
         
         this.posx = 0
         this.posy = window.innerHeight - 150
+        this.addEventListener("click", (e : MouseEvent) => this.handleMouseClick(e))
+        this.speed = Math.floor(Math.random() * Math.floor(5) + 1)
     
         
     }
 
     public update():void {
         if (this.posx > window.innerWidth + 150) {
-            this.posx = -150
+            this.posx = Math.floor(Math.random() * Math.floor(400) -650)
+            this.speed = Math.floor(Math.random() * Math.floor(5) + 1)
         }
-        this.posx++
+        this.posx = this.posx + this.speed;
         this.style.transform = `translate(${this.posx}px, ${this.posy}px)`
     }
+
+    private handleMouseClick(e:MouseEvent) {
+        this.posx = Math.floor(Math.random() * Math.floor(400) -650)
+        this.speed = Math.floor(Math.random() * Math.floor(5) + 1)
+        Game.instance().buildBuilding();
+    }
+
+
 }
 
 window.customElements.define("car-component", Car as any)
